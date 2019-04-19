c89bf826c648b7ed11ebbd53d7db52f1d5d5a4f1
ALMAZ <ALMAZ@DESKTOP>
2019-04-19 18:02:36 +0500

    Добавьте файлы проекта.

---
+++ WebUI/App_Start/RouteConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebUI
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: null,
+                url: "",
+                defaults: new { controller = "Game", action = "List", category = (string)null, page = 1 }
+            );
+
+            routes.MapRoute(
+                name: null,
+                url: "Page{page}",
+                defaults: new { controller = "Game", action = "List", category = (string)null },
+                constraints: new { page = @"\d+" }
+            );
+
+            routes.MapRoute(
+                name: null,
+                url: "{category}",
+                defaults: new { controller = "Game", action = "List", page = 1 }
+            );
+
+            routes.MapRoute(
+                name: null,
+                url: "{category}/Page{page}",
+                defaults: new { controller = "Game", action = "List" },
+                constraints: new { page = @"\d+" }
+            );
+
+            routes.MapRoute(null, "{controller}/{action}");
+        }
+    }
+}
